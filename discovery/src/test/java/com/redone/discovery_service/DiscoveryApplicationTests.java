9ff959ca436448d77becf8d3776c3446f808ae25
RedOuaneAO <redouaneao97@gmail.com>
2024-03-14 12:05:38 +0100

    create gateWay , dicovery,config-server and product service

---
+++ discovery/src/test/java/com/redone/discovery_service/DiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redone.discovery_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
